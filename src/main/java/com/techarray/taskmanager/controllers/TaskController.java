9272395a01e656f05c9f2882c6acc9b9f5c035bf
Nishant shaligram <nishant.shaligram@gmail.com>
2024-11-07 22:05:53 +0530

    Add task controller method added with Exception handler

--- src/main/java/com/techarray/taskmanager/controllers/TaskController.java
+++ src/main/java/com/techarray/taskmanager/controllers/TaskController.java
@@ -3,13 +3,17 @@ package com.techarray.taskmanager.controllers;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techarray.taskmanager.dto.CreateTaskDTO;
+import com.techarray.taskmanager.dto.ErrorResponseDTO;
 import com.techarray.taskmanager.entities.TaskEntity;
 import com.techarray.taskmanager.services.TaskService;
 
+import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,7 +35,7 @@ public class TaskController {
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<TaskEntity> getTaskById(int id) {
+    public ResponseEntity<TaskEntity> getTaskById(@PathVariable("id") Integer id) {
         var task = taskService.getTaskById( id );
         if( task == null ){
             return ResponseEntity.notFound().build();
@@ -39,11 +43,18 @@ public class TaskController {
         return ResponseEntity.ok( task );
     }
 
-    @PostMapping("path")
-    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) {
+    @PostMapping("")
+    public ResponseEntity<TaskEntity> addTask(@RequestBody CreateTaskDTO body) throws ParseException {
         var task = taskService.addTask(body.getTitle(), body.getDescription(), body.getDeadline());
         return ResponseEntity.ok(task);
     }
     
-    
+    @ExceptionHandler(ParseException.class)
+    public ResponseEntity<ErrorResponseDTO> handleErrors(Exception e){
+        if( e instanceof ParseException ) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO("Invalid date format"));
+        }
+        e.printStackTrace();
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO("Internal Server Error"));
+    }
 }
