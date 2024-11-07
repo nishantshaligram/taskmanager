e0d43d20837338c4977731c6f861dcbb5f859843
Nishant shaligram <nishant.shaligram@gmail.com>
2024-11-07 18:19:19 +0530

    Task Service added.

---
+++ src/main/java/com/techarray/taskmanager/services/TaskService.java
@@ -0,0 +1,38 @@
+package com.techarray.taskmanager.services;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.techarray.taskmanager.entities.TaskEntity;
+
+@Service
+public class TaskService {
+    private ArrayList<TaskEntity> tasks = new ArrayList<>();
+    private int taskId = 1;
+
+    public void addTask( String title, String description, String deadline ) {
+        TaskEntity task = new TaskEntity();
+        task.setId(taskId);
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setDeadline(new Date( deadline ));
+        task.setCompleted(false);
+        tasks.add( task );
+        taskId++;
+    }
+
+    public ArrayList<TaskEntity> getTasks() {
+        return tasks;
+    }
+
+    public TaskEntity getTaskById(int id) {
+        for( TaskEntity task : tasks ){
+            if( task.getId() == id ) {
+                return task;
+            }
+        }
+        return null;
+    }
+}
